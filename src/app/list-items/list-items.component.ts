efdae957942c16a7a3562f62075ecd71581679df
Arturo Aguado Arnold <arturo.aguado.arnold@everis.com>
2018-11-09 21:06:01 +0100

    Last changes

--- src/app/list-items/list-items.component.ts
+++ src/app/list-items/list-items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../models/items';
 
 @Component({
   selector: 'devschool-list-items',
@@ -7,46 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListItemsComponent implements OnInit {
 
-  items: any[];
+  items: Item[];
 
   constructor() {
     this.items = [
       {
         id: 1,
         name: 'armadura',
+        description: 'Es una armadura estupenda',
         price: 20,
         currency: 'EUR',
         available: true,
+        img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
       },
       {
         id: 2,
         name: 'escudo',
+        description: 'Es un escudo magnífico',
         price: 29.6,
         currency: 'EUR',
         available: false,
+        img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
       },
       {
         id: 3,
         name: 'casco',
+        description: 'Es un casco genial',
         price: 15.9991,
         currency: 'USD',
         available: false,
+        img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
       },
       {
         id: 4,
         name: 'pala',
+        description: 'Es una pala estratosférica',
         price: 9.99,
         currency: 'USD',
         available: true,
+        img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
       },
       {
         id: 5,
         name: 'caballo',
+        description: 'Es un caballo muy cariñoso',
         price: 980,
         currency: 'USD',
         available: true,
+        img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
       }
     ];
+
   }
 
   ngOnInit() {
