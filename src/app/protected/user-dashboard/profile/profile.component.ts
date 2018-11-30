a03ad749e4dc421e117f321a2b39b7d861126b43
Arturo Aguado Arnold <arturo.aguado.arnold@everis.com>
2018-11-30 12:53:34 +0100

    Adde on submit, observable to formChanges and setValue/patchValue

--- src/app/protected/user-dashboard/profile/profile.component.ts
+++ src/app/protected/user-dashboard/profile/profile.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
 import { ProfileService } from '../services/profile.service';
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'devschool-profile',
@@ -8,13 +10,51 @@ import { ProfileService } from '../services/profile.service';
   styleUrls: ['./profile.component.css'],
   providers: [ProfileService]
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnDestroy {
 
-  reactiveProfileForm = this.formBuilder.group({
-    name: ['', Validators.required],
-    lastNames: ['', Validators.required]
-  });
+  _subscription: Subscription;
 
-  constructor(private formBuilder: FormBuilder, private profileService: ProfileService) { }
+  reactiveProfileForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private profileService: ProfileService) {
+    this.reactiveProfileForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      lastNames: ['', Validators.required]
+    });
+    
+    this._subscription = this.reactiveProfileForm.valueChanges
+    .pipe(map(formChanges => formChanges.name))
+    .subscribe(
+      nameList => {
+        console.log(nameList);
+      }
+    );
+  }
+
+  onSubmit(): void {
+    console.log('submitted!');
+
+    setTimeout(() => {
+      this.reactiveProfileForm.setValue({
+        name: 'Arturo',
+        lastNames: 'Aguado'
+      });
+
+      setTimeout(() => {
+        
+        this.reactiveProfileForm.setValue({
+          lastNames: 'Aguado Arnold'
+        });
+
+        // this.reactiveProfileForm.patchValue({
+        //   lastNames: 'Aguado Arnold'
+        // });
+
+      }, 10000);
+    }, 10000);
+  }
+
+  ngOnDestroy() {
+    this._subscription.unsubscribe();
+  }
 }
